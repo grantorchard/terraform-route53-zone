8f2a06dfe2388728d51c7d50ebe860c5e0e0ca20
Grant Orchard <go@hashicorp.com>
2019-11-22 08:56:13 +1100

    Removed double quotes from variable 'types'

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable tld {
-    type = "string"
+    type = string
     description = "Top Level Domain that will be used to generate subdomains against. Requires a trailing dot."
 }
 
 variable zone {
-    type = "list"
+    type = list
     description = "The zone to be created in the top level domain."
 }
 
 variable zone_tags {
-    type = "map"
+    type = map
     default = {}
 }
